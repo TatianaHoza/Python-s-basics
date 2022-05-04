9d57855bc4578b72f72465388748b012ebe9b059
TatianaHoza <91697673+TatianaHoza@users.noreply.github.com>
2022-05-04 17:09:13 +0300

    Revert "Hoza tatiana dz 6"

--- Hoza_Tatiana_dz_5/Exircise 3.py
+++
@@ -1,5 +0,0 @@
-from itertools import zip_longest, islice
-tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
-klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
-my_gen = ((tutors, klasses) for tutors, klasses in zip_longest(tutors, klasses))
-print(*islice(my_gen, 8))
